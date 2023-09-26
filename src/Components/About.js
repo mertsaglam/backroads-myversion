c25836da79e27a9588f6444ae7b2a5c2678872be
mertsaglam <mertsaglam@marun.edu.tr>
2023-09-26 11:34:41 +0300

    hadi baba bırakabilirsin

--- src/Components/About.js
+++ src/Components/About.js
@@ -23,7 +23,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          <a href="#" className="btn" target="_blank" rel="noreferrer">
             read more
           </a>
         </article>
